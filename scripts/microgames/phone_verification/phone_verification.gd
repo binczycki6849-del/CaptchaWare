0dfb1b9d3a344d92872b9672fea1b1fd4f6b40a2
binczycki6849-del <binczycki6849@gmail.com>
2026-08-22 20:20:19 -0500

    feat: aggressive conversion for phone_verification.gd (Godot4 -> 3.5 await/tween/preload compatibility)

--- scripts/microgames/phone_verification/phone_verification.gd
+++ scripts/microgames/phone_verification/phone_verification.gd
@@ -1,21 +1,21 @@
 extends Microgame
 
-const PHONE_CALL_WINDOW = preload("uid://bnyk2f5trja0r")
+# Godot 3.5 compatibility pass
+# NOTE: verify this path in-editor if the scene lives elsewhere.
+const PHONE_CALL_WINDOW = preload("res://instances/phone_call_window.tscn")
 
-@onready var camera = get_tree().get_first_node_in_group("camera")
-@onready var phonenumber_label: Label = $phonenumber
+onready var camera = get_tree().get_nodes_in_group("camera")[0] if get_tree().get_nodes_in_group("camera").size() > 0 else null
+onready var phonenumber_label = $phonenumber
 
-var phone_number: int = 0
+var phone_number = 0
 
-var real_number_calling : bool = false
-var has_answered: bool = false
+var real_number_calling = false
+var has_answered = false
 
-var number_of_calls : int = 0
-var how_many_fake_calls : int = 1
+var number_of_calls = 0
+var how_many_fake_calls = 1
 
-# Called when the node enters the scene tree for the first time.
-
-var fake_number_type : PackedStringArray = [
+var fake_number_type = [
 	"ding",
 	"erynn",
 	"hira",
@@ -33,20 +33,16 @@ var fake_number_type : PackedStringArray = [
 	"jemi"
 ]
 
-@onready var popup: AudioStreamPlayer = $popup
+onready var popup = $popup
 
 func _ready() -> void:
 	phonenumber_label.text = generate_number()
-
 	how_many_fake_calls = randi_range(1, 3)
 
-
-func pop_up_window() -> void: #spawn window
-	var phone_call_window : Control = PHONE_CALL_WINDOW.instantiate()
-	
+func pop_up_window() -> void:
+	var phone_call_window = PHONE_CALL_WINDOW.instance()
 	phone_call_window.call_answered.connect(on_call_answered)
 	phone_call_window.call_declined.connect(on_call_declined)
-	
 	add_child(phone_call_window)
 
 	set_camera_shake.emit(10, 0.5)
@@ -60,24 +56,20 @@ func pop_up_window() -> void: #spawn window
 		phone_call_window.phone_audio.stream = get_phone_call_audio()
 	number_of_calls += 1
 
-func get_phone_call_audio() -> AudioStream:
-	var path : String = "res://sounds/microgames/phone_verification/"
-
+func get_phone_call_audio():
+	var path = "res://sounds/microgames/phone_verification/"
 	if real_number_calling:
 		path += "automated_message.mp3"
 	else:
 		path += "fake_numbers/" + fake_number_type[randi_range(0, fake_number_type.size() - 1)]
-		var audio_phone_final : PackedStringArray = get_file_list(path, ".mp3")
+		var audio_phone_final = get_file_list(path, ".mp3")
 		path += "/" + audio_phone_final[randi_range(0, audio_phone_final.size() - 1)]
-	
 	return load(path)
 
 func generate_number() -> String:
-	var number_text: String = ""
-	phone_number = randi_range(9999999999, 1000000000)
-	
-	var number_array : PackedStringArray = str(phone_number).split()
-	
+	var number_text = ""
+	phone_number = randi_range(1000000000, 9999999999)
+	var number_array = str(phone_number).split("")
 	number_text = "+1 ("
 	for i in range(10):
 		number_text += number_array[i] + get_phone_format(i)
@@ -96,34 +88,34 @@ func canSkip() -> bool:
 	return false
 
 func isWinning() -> bool:
-	return has_answered && real_number_calling
+	return has_answered and real_number_calling
 
 func on_call_answered() -> void:
 	has_answered = true
-
-	await get_tree().create_timer(0.5).timeout
-
+	yield(get_tree().create_timer(0.5), "timeout")
 	force_end_mircogame()
 
+func _make_tween() -> Tween:
+	var t = Tween.new()
+	add_child(t)
+	return t
+
 func on_transition_complete() -> void:
-	var cam_tween := create_tween()
-	cam_tween.tween_property(camera, "zoom", Vector2.ONE * 1.80, 4.0).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CIRC)	
-
-	await get_tree().create_timer(randf_range(2.0, 4.0)).timeout
-
-	cam_tween.stop()
-	cam_tween = create_tween()
-	
-	cam_tween.tween_property(camera, "zoom", Vector2.ONE * 1.43, 0.2).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO).from(Vector2.ONE * 2)
+	if camera != null:
+		var cam_tween = _make_tween()
+		cam_tween.interpolate_property(camera, "zoom", camera.zoom, Vector2.ONE * 1.80, 4.0, Tween.TRANS_CIRC, Tween.EASE_IN)
+		cam_tween.start()
+	yield(get_tree().create_timer(rand_range(2.0, 4.0)), "timeout")
+	if camera != null:
+		var cam_tween_back = _make_tween()
+		cam_tween_back.interpolate_property(camera, "zoom", camera.zoom, Vector2.ONE * 1.43, 0.2, Tween.TRANS_EXPO, Tween.EASE_OUT)
+		cam_tween_back.start()
 	pop_up_window()
 
 func on_call_declined() -> void:
 	if real_number_calling:
-		await get_tree().create_timer(0.5).timeout
+		yield(get_tree().create_timer(0.5), "timeout")
 		force_end_mircogame()
 		return
-	
-	await get_tree().create_timer(1.5).timeout
-
+	yield(get_tree().create_timer(1.5), "timeout")
 	pop_up_window()
-	
